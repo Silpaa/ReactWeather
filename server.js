8a98c930ccdd6fb60eeaf063f85f7170cfb9647e
Narasimha Muddana <Silpakumar@apples-MBP.home>
2017-03-20 11:54:30 -0400

    App deploying to heroku

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next){
-  if(){
-    if(req.headers['x-forwarded-proto'] === 'http')
+
+  if(req.headers['x-forwarded-proto'] === 'http'){
     next();
   } else {
     res.redirect('http://'+ req.hostname + req.url);
